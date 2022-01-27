3a1aa0f15890faa808afb5df1617f67113301f72
Lara Daniel <88368957+laradaniel@users.noreply.github.com>
2022-01-27 16:29:40 -0300

    Alerts com react-toastify

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { addToken } from '../../../store/tokens/actions';
+import {toast} from 'react-toastify';
 
 function Navbar() {                            
     const token = useSelector<TokenState, TokenState["tokens"]>(
@@ -16,7 +17,16 @@ function Navbar() {
 
     function goLogout(){
         dispatch(addToken(''));
-        alert("Usuário deslogado")
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
         history.push('/login')
     }
 
